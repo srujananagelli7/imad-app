b220b2a89933d3e4ad27ccf7832f6ac1a68b1c23
srujananagelli7 <30927684+srujananagelli7@users.noreply.github.com>
2017-08-14 19:20:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article_one',function(req, res){
+app.get('/article-one',function(req, res){
    res.send(createTemplate(article-one));
 });
 app.get('/article-two',function(req, res){

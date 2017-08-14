5c3343942009ffcef4ee66a12ced4ec0cac05cc0
srujananagelli7 <30927684+srujananagelli7@users.noreply.github.com>
2017-08-14 16:48:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one',function(req, res){
+app.get('/article-one',function(req, res){
     res.send('Article one requested and will be served');
 });
-app.get('article-two',function(req, res){
+app.get('/article-two',function(req, res){
     res.send('Article two requested and will be served');
 });
-app.get('article-three',function(req, res){
+app.get('/article-three',function(req, res){
     res.send('Article three requested and will be served');
 });
 app.get('/ui/style.css', function (req, res) {

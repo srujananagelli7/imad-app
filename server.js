03727b7c89873774ab2e157ac9c63e03c0302b3b
srujananagelli7 <30927684+srujananagelli7@users.noreply.github.com>
2017-08-14 18:44:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var htmlTemplate=`{
     <html>
     <head>
         <title>
-            article one:srujana
+            ${title}
         </title>
    
          <meta name="viewport" content ="width-device-width, initial-scale-1"/>

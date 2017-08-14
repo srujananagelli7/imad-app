bb13c75fec09781e24c975143a9254109137f32f
srujananagelli7 <30927684+srujananagelli7@users.noreply.github.com>
2017-08-14 19:19:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 var article_one= {
     title:'srujana',
-    heading:'article one',
+    heading:'article_one',
     date:'sep 5,2016',
     content:`
      <p> 
@@ -57,7 +57,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req, res){
+app.get('/article_one',function(req, res){
    res.send(createTemplate(article_one));
 });
 app.get('/article-two',function(req, res){
